383582327039e152a7881b2f3415741cfab8df8a
artur <alatorca22@alumnes.ub.edu>
2024-10-25 21:56:19 +0200

    Menu Registro

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
     <div v-if="showToast" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-      <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+      <div v-if="toastError" class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
         </svg>
         <span class="sr-only">Error icon</span>
       </div>
+
+      <div v-else class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+        </svg>
+        <span class="sr-only">Check icon</span>
+      </div>
+
       <div class="ms-3 text-sm font-normal">{{ toastMessage }}</div>
       <button type="button" @click="hideToast" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">
         <span class="sr-only">Close</span>
@@ -278,6 +286,7 @@ export default {
       // Toast
       showToast: false,
       toastMessage: '',
+      toastError: false
     };
   },
   mounted() {
@@ -308,11 +317,13 @@ export default {
         else this.confirmPasswordVisible = false;
       }
     },
-    showToastWithMessage(message) {
+    showToastWithMessage(message, error, callback) {
       this.toastMessage = message;
       this.showToast = true;
+      this.toastError = error;
       setTimeout(() => {
         this.showToast = false;
+        if (callback) callback();
       }, 5000);
     },
     isValidEmail(email) {
@@ -373,17 +384,17 @@ export default {
     },
     async handleSubmit() {
       if (this.password !== this.confirmPassword) {
-        this.showToastWithMessage('Las contraseñas no coinciden.');
+        this.showToastWithMessage('Passwords do not match.', true);
         return;
       }
 
       if(this.passwordStrength !== 5){
-        this.showToastWithMessage('La contraseña no es suficientemente segura.');
+        this.showToastWithMessage('Password is not strong enough.', true);
         return;
       }
 
       if (!this.isValidEmail(this.email)) {
-        this.showToastWithMessage('Incorrect email format');
+        this.showToastWithMessage('Incorrect email format', true);
         return;
       }
 
@@ -404,18 +415,19 @@ export default {
         if (response.status === 200) {
             console.log('Éxito:', response.data);
             this.showToastWithMessage(
-                response.data.message || 'Registro exitoso.',
-                () => this.$router.push('/login')
+              response.data.message || 'Registration successful.',
+              false,
+              () => this.$router.push('/login')
             );
         }
       } catch (error) {
         if (error.response && error.response.status === 409) {
-            const errorMessage = error.response.data.message;
-            this.showToastWithMessage(errorMessage);
+            const errorMessage = error.response.data.error.message;
+            this.showToastWithMessage(errorMessage, true);
         } else {
-            this.showToastWithMessage('Error en la solicitud. Intente nuevamente.');
+            this.showToastWithMessage('Request failed. Please try again.', true);
         }
-        console.error('Error en la solicitud:', error);
+        console.error('Request failed.', error);
     }
     }
   },
