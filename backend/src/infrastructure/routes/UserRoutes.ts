2334b527a3cc1ffca7958ed3de06ccbe5634cf97
Maria Pérez <mperezpulles@gmail.com>
2024-10-26 16:45:00 +0200

    [FIX] added openapi documentation

--- backend/src/infrastructure/routes/UserRoutes.ts
+++ backend/src/infrastructure/routes/UserRoutes.ts
@@ -6,6 +6,14 @@ const router = Router();
 router.put('/:userId', (req: Request, res: Response, next: NextFunction) => UserController.updateUser(req, res, next));
 router.delete("/:userName", (req: Request, res: Response, next: NextFunction) => UserController.deleteUser(req, res, next));
 router.get("/:userName", (req: Request, res: Response, next: NextFunction) => UserController.getUser(req, res, next));
-router.post("/register", (req: Request, res: Response, next: NextFunction) => UserController.registerUser(req, res, next));
+router.post("/register", (req: Request, res: Response, next: NextFunction) => {
+    /* #swagger.parameters['body'] = {
+        in: 'body',
+        description: 'User data.',
+        required: true,
+        schema: { $ref: '#/definitions/CreateUser' }
+    } */
+    UserController.registerUser(req, res, next);
+});
 
 export default router;
