2bb2b91f92dfa29c6fc67ce633fefceba2f03ca7
JINGYE1D <jingye1d@gmail.com>
2024-11-18 12:30:28 +0100

    Fix movies in the app.ts (added import and the routes)

--- backend/src/main/app.ts
+++ backend/src/main/app.ts
@@ -4,6 +4,7 @@ import "reflect-metadata"
 import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "../../docs/openapi.json";
 import userRoutes from "../infrastructure/routes/UserRoutes";
+import movieRoutes from "../infrastructure/routes/MovieRoutes"
 import errorHandler from "../infrastructure/config/ErrorHandler";
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors<Request>({
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 app.use("/user", userRoutes);
+app.use("/movie", movieRoutes)
 
 // Este middleware de manejo de errores debe ir al final
 app.use(errorHandler);
