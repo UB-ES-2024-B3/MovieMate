6b81258f20dd9a65b93d79676b0ce1afa90edd2a
artur <alatorca22@alumnes.ub.edu>
2024-12-14 13:39:49 +0100

    Test Functional

--- backend/_test/functional_tests/Users/userProfileUpdate.test.ts
+++ backend/_test/functional_tests/Users/userProfileUpdate.test.ts
@@ -1,26 +1,59 @@
 import request from 'supertest';
-import app from '../../../src/main/app'; // Ajusta la ruta
+import app from '../../../src/main/app';
+import {createTestUser, deleteTestUser, getUserTest} from "../../test_utils/testUtilsUsers";
+import {afterAll, beforeAll, expect, it} from "@jest/globals";
+import {PostgreTypeOrmDataSource} from "../../../src/main/config/postgreDatabaseTypeOrm"; // Ajusta la ruta
 
 describe('Functional Tests for User Profile Update', () => {
   let userToken: string;
+  let userName: string;
+  let userId: string;
+  let existingUser;
 
   beforeAll(async () => {
-    const loginResponse = await request(app)
-      .post('/user/login')
-      .send({ userName: 'testUser', password: 'Password123!' });
-    userToken = loginResponse.body.token;
+      if (!PostgreTypeOrmDataSource.isInitialized) {
+          await PostgreTypeOrmDataSource.initialize();
+      }
+
+      existingUser = await getUserTest('TestUser');
+      if(!existingUser){
+          const user = await createTestUser();
+          userId = user.user.id;
+      }else{
+          userId = existingUser.user.id;
+      }
+
+      const loginResponse = await request(app)
+        .post('/user/login')
+        .send({
+          userName: 'TestUser',
+          password: '1234#User',
+        });
+      userName = "TestUser"
+      userToken = loginResponse.body;
+  });
+
+  afterAll(async () => {
+      if (PostgreTypeOrmDataSource.isInitialized) {
+          await PostgreTypeOrmDataSource.destroy();
+      }
+      await deleteTestUser(userName);
   });
 
   it('should allow the user to update their profile', async () => {
-    const updatedData = { userName: 'updatedTestUser', email: 'updated@example.com' };
+    const updatedData = {
+      userName: 'updatedTestUser',
+      email: 'updated@example.com' };
+
 
     const response = await request(app)
-      .put('/user/update/testUser')  // Asegúrate de usar un userId válido
+      .put(`/user/update/${userId}`)
       .set('Authorization', `Bearer ${userToken}`)
       .send(updatedData);
 
+    userName = "updatedTestUser"
     expect(response.status).toBe(200);
-    expect(response.body.message).toBe('User profile updated');
-    expect(response.body.data.userName).toBe(updatedData.userName);
+    //Return New token
+    expect(typeof response.body).toBe('string');
   });
 });
